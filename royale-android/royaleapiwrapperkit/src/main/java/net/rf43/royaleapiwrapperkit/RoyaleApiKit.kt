6eac1a146b3b321edc03af6924fdba30f727f05c
Rich Friedel <rich.friedel@willowtreeapps.com>
2019-01-23 01:53:54 -0500

    Adding some test models and start standing up the API layer

--- royale-android/royaleapiwrapperkit/src/main/java/net/rf43/royaleapiwrapperkit/RoyaleApiKit.kt
+++ royale-android/royaleapiwrapperkit/src/main/java/net/rf43/royaleapiwrapperkit/RoyaleApiKit.kt
@@ -1,5 +1,28 @@
 package net.rf43.royaleapiwrapperkit
 
-class RoyaleApiKit {
+import android.util.Log
+import net.rf43.royaleapiwrapperkit.api.RawTopPlayerModel
+import net.rf43.royaleapiwrapperkit.api.RoyaleApiDataService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
+class RoyaleApiKit(developerKey: String) {
+
+    private val royaleApiDataService = RoyaleApiDataService.create(developerKey)
+
+    fun getTopPlayers(max: Int = 1000) {
+        royaleApiDataService.topPlayers().enqueue(object : Callback<List<RawTopPlayerModel.RawTopPlayer>> {
+            override fun onFailure(call: Call<List<RawTopPlayerModel.RawTopPlayer>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(
+                call: Call<List<RawTopPlayerModel.RawTopPlayer>>,
+                response: Response<List<RawTopPlayerModel.RawTopPlayer>>) {
+                Log.i("ROYALE_API_KIT", "onResponse => list of top players returned")
+                Log.w("ROYALE_API_KIT", response.body().toString())
+            }
+        })
+    }
 }
