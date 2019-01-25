28be105ab0cd0365d8e29f39b8ae50d7115ed5e5
Rich Friedel <rich.friedel@gmail.com>
2019-01-25 00:18:11 -0500

    Fixed gradle and tests

--- royale-android/app/src/test/java/net/rf43/royaleapiwrapper/ExampleUnitTest.kt
+++ royale-android/app/src/test/java/net/rf43/royaleapiwrapper/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package net.rf43.royaleapiwrapper
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- royale-android/royaleapiwrapperkit/src/test/java/net/rf43/royaleapiwrapperkit/ExampleUnitTest.java
+++ royale-android/royaleapiwrapperkit/src/test/java/net/rf43/royaleapiwrapperkit/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package net.rf43.royaleapiwrapperkit;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
